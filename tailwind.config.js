05cb6f29541fc3002e4f9b83ba838831b8479ac8
Tony S <sanjayatony@gmail.com>
2020-10-21 11:29:15 +0700

    add index page and custom taiwlind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "components/**/*.vue",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "nuxt.config.js",
+      // TypeScript
+      "plugins/**/*.ts",
+      "nuxt.config.ts"
+    ]
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Josefin Sans", "sans-serif"]
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
